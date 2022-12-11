9a9c550c454b0fcae05f9cce6dc8b31377ef4cd4
Parthiba Mukhopadhyay <89331202+Parthiba-Mukhopadhyay@users.noreply.github.com>
2022-12-11 21:32:46 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,23 +1,23 @@
 let str="";
-let buttons1=document.querySelectorAll('.button');
-Array.from(buttons1).forEach((button)=>{
+let buttons1=document.querySelectorAll('.button'); //to select all query having class button
+Array.from(buttons1).forEach((button)=>{ //Array listing all the instances
     button.addEventListener('click',(e)=>{
-    if(e.target.innerHTML=='=')
+    if(e.target.innerHTML=='=') //It evaluates the string
     {
         str=eval(str);
         
     }
-    else if(e.target.innerHTML=='AC')
+    else if(e.target.innerHTML=='AC') //to perform all clear operation
     {
         str="";
     }
-    else if(e.target.innerHTML=='D')
+    else if(e.target.innerHTML=='D') // to perform delete operation
     {
         str=str.substring(0,str.length-1);
     }
     else{
         str=str+e.target.innerHTML;
     }
-    document.querySelector('input').value=str;
+    document.querySelector('input').value=str; //To assign the input box value with the value of string
     })
 })
